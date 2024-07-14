e8d5dbf43909f58a8c3c398796c89ed7f8ea4634
Morley Cellan <morley@morce.dev>
2024-07-14 19:43:28 +0800

    Update task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const listGallery = array => {
+  return array
+    .map(
+      ({ preview, original, description }) => `
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    )
+    .join('');
+};
+galleryContainer.insertAdjacentHTML('beforeend', listGallery(galleryItems));
+const galleryHandler = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+galleryHandler.on('show.simplelightbox');
